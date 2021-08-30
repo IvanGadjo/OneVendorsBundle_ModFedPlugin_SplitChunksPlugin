f78bf28ab56e174833027bfdf70131b839d6dc6f
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2021-08-30 15:54:49 +0200

    added lodash to remote, change structure of host

---
+++ Remote_App/src/sharedModules/mySharedFunction.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+
+export const myFunction = () => {
+    let sampleArray = [1,1,2,2,2,3,4,5,5,6];
+    let sortedArray = _.sortedUniq(sampleArray);
+    console.log('My resulting array: ' + sortedArray);
+}
+
+// (() => {
+//     myFunction();
+// })()
