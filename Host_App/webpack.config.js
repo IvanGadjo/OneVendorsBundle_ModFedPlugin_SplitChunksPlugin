20504ee89b68a8efc787152bacbd351416a97ea2
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2021-08-30 15:55:32 +0200

    Change wp config because of new structure of host

--- Host_App/webpack.config.js
+++ Host_App/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/mainLogic.js',
+  entry: './src/bootstrap.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
