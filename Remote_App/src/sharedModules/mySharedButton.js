cf9f51ba3bef32c206d30a8b5f67db9774a8079a
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2021-11-05 12:03:01 +0100

    Move import of lodash on top of file

--- Remote_App/src/sharedModules/mySharedButton.js
+++ Remote_App/src/sharedModules/mySharedButton.js
@@ -1,3 +1,5 @@
+import * as d3 from 'd3';
+
 // create & fill elements
 let d3Btn = document.createElement('button');
 d3Btn.setAttribute('id','btn-d3');
@@ -8,5 +10,4 @@ let container = document.getElementsByTagName('body');
 container[0].appendChild(d3Btn);
 
 // use d3
-import * as d3 from 'd3';
 d3.select('#btn-d3').style('color','orange');   // change color of text to orange
